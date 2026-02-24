dc2aad6789a4c9034226583ae857ec80819ec021
shweta0055 <kumarishwetajmpdhr@gmail.com>
2026-02-25 00:17:25 +0530

    import os

--- backend/hrms_backend/settings.py
+++ backend/hrms_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,7 +79,6 @@ WSGI_APPLICATION = "hrms_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import os
 
 DATABASES = {
     'default': {
